ab8c09dc9ca5af3c882f54b0665e1d412d6c3df1
karanns19 <karanns.aero19@gmail.com>
2024-01-15 18:12:59 +0530

    modified commit

--- paraphrasing.js
+++ paraphrasing.js
@@ -1,3 +1,4 @@
+// Imported different libraries
 const readline = require('readline');
 const axios = require('axios');
 
@@ -6,29 +7,27 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+// Paraphrasing Function
 async function paraphraseText(text, option) {
   const words = text.split(' ');
 
   const paraphrasedWords = await Promise.all(
     words.map(async (word) => {
       try {
-        // Retrieve synonyms for each word using datamuse API
         const synonyms = await getSynonyms(word, option);
-
-        // Use a synonym if available, otherwise keep the original word
         const paraphrasedWord = synonyms.length > 0 ? synonyms[0].word : word;
 
         return paraphrasedWord;
       } catch (error) {
         console.error(error.message);
-        return word; // Keep the original word if an error occurs
+        return word;
       }
     })
   );
 
   return paraphrasedWords.join(' ');
 }
-
+// Get Synonyms Function using API
 async function getSynonyms(word, option) {
   let apiUrl = `https://api.datamuse.com/words?rel_syn=${word}`;
 
@@ -45,7 +44,7 @@ async function getSynonyms(word, option) {
   return response.data;
 }
 
-
+// Nodejs CLI Readlines Function
 rl.question('Enter the text you want to paraphrase: ', async (text) => {
   rl.question('Select an option (Professional, Creative, Urgent): ', async (option) => {
     try {
@@ -59,6 +58,7 @@ rl.question('Enter the text you want to paraphrase: ', async (text) => {
   });
 });
 
+// Exited CLI
 rl.on('close', () => {
   process.exit(0);
 });
